ed5ae61002eec2ae0e50ea718cff58c47e31becf
Kader Oğuz <2023123042@cumhuriyet.edu.tr>
2025-11-26 12:53:35 +0300

    Add files via upload

---
+++ AdvanceSortApp.java
@@ -0,0 +1,20 @@
+package ArraySort;
+
+public class AdvanceSortApp {
+
+        public static void main(String[] args) {
+            int maxSize=100;
+            ArraySort dizi = new ArraySort(maxSize);
+
+            for(int i= 0; i<10; i++){
+                dizi.insert((int)(java.lang.Math.random()*99));
+
+            }
+            dizi.display();
+           // dizi.mergeSort();
+            dizi.shellSort();
+           // dizi.quickSort();
+            dizi.display();
+        }
+    }
+
